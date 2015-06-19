3a2e342c0188b3cd18d2b764e78627680919b56c
Katie Gaudin <katiegaudin2@GaudinMacintosh.local>
2015-06-18 18:06:01 -0700

    added play to five code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -61,12 +61,21 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
-function playToFive() {
+function playToFive(playerWins, computerWins) {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    while ((playerWins < 5) || (computerWins < 5)) {
+        getWinner();
+        if (winner == "computer") {
+            computerWins += 1;
+        }
+        else if (winner == "player") {
+            playerWins += 1;
+        }
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
+} console.log ("Player chose " + playerMove + "while Computer chose " + computerMove);
+  console.log ("The score is currently " + playerWins + " to " + computerWins);  
 }
-
